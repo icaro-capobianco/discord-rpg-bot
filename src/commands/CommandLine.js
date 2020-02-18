929af7a5bf83fe8bb98f4eb465c775f7ab822d43
Icaro C. Capobianco <icaro.capobianco@gmail.com>
2020-02-18 16:27:47 -0300

    Trying to maintain some f patterns

--- src/commands/CommandLine.js
+++ src/commands/CommandLine.js
@@ -1,13 +1,5 @@
 const Discord = require('discord.js');
-
-const CommandsTree = {
-    character: { name: 'character', children: {
-        create: { name: 'create', description: 'Example: \ngm! character create [character_name]\n[attribute_name]:[attribute_value]\n-----\ncheck your server sheet making channel for more info.' },
-        update: { name: 'update', description: 'Example: \ngm! character update [character_name]\n[attribute_name]:[attribute_value]\n-----\ncheck your server sheet making channel for more info.' },
-        addto: { name: 'addto', description: 'Example: \ngm! character addto [character_name]\n[attribute_name]:[attribute_value]\n-----\nthis will result in a list of values. For example if your attribute is currency:1gp and you add currency:1sp you will have currency: 1gp, 1sp.' },
-        increment: { name: 'increment', description: 'Example: \ngm! character increment [character_name]\n[attribute_name]:[attribute_value]\n-----\nthis will only work to numeric values, it will increment the attibute by the desired amount, but only if the current attribute value is also numeric.' },
-    } },
-}
+const CommandsTree = require( './CommandTree' )
 
 class CommandLine {
 
@@ -19,7 +11,7 @@ class CommandLine {
 
     findCommand(commandName) {
         let command = CommandsTree[commandName]
-        if( ! command ) {
+        if(! command) {
             throw new `${commandName} is not a valid command`
         }
         return command;
@@ -28,7 +20,7 @@ class CommandLine {
     execute() {
         console.log('Reading CommandLine')
         let commandName = this.commands[1] || false;
-        if( ! commandName ) {
+        if(! commandName) {
             return this.messageCommands(CommandsTree)
         }
         console.log(`First command is: ${commandName}`)
@@ -40,7 +32,7 @@ class CommandLine {
 
     executeCommand(command, parameters) {
         console.log(`Reading command ${command.name}`)
-        if( command.children ) {
+        if(command.children) {
 
             console.log(`Command ${command.name} has children`)
             var childName = parameters[0] || false
@@ -60,17 +52,17 @@ class CommandLine {
     }
 
     messageCommands(commands) {
-        var reply = commands.reduce( ( a, e ) => {
-            if( a === '' )
+        var reply = commands.reduce(( a, e ) => {
+            if(a === '')
                 a = `here are some options you can try:`
             return `${a}\n**${e.name}:**\n${e.description}\n`
-        }, '' )
+        }, '')
 
-        this.message.discordMessage.reply( reply ).then( sent => {
-            console.log( `Sent a reply to ${sent.author.username}` )
-        } ).catch( err => {
-            console.log( err )
-        } )
+        this.message.discordMessage.reply(reply).then(sent => {
+            console.log(`Sent a reply to ${sent.author.username}`)
+        }).catch( err => {
+            console.log(err)
+        })
     }
     
 }
--- src/functions.js
+++ src/functions.js
@@ -1,21 +1,21 @@
 const MessageWrap = require('./models/MessageWrap');
-const CommandLine = require('./models/CommandLine');
+const CommandLine = require('./commands/CommandLine');
 
-function handleMessage( discordMessage ) {
-    console.log( 'Message being handled' )
-    let message = new MessageWrap( discordMessage )
+function handleMessage(discordMessage) {
+    console.log('Message being handled')
+    let message = new MessageWrap(discordMessage)
     if( message.isCommand() ) {
-        handleCommand( message )
+        handleCommand(message)
     }
 }
 
-function handleCommand( message ) {
-    console.log( 'Command being handled' )
-    let command = new CommandLine( message )
+function handleCommand(message) {
+    console.log('Command being handled')
+    let command = new CommandLine(message)
     try {
         command.execute()
     } catch (error) {
-        message.reply( error )
+        message.reply(error)
     }
 }
 
